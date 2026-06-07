323c613d750e201f3091ab7391dafa7ec5f0f0da
swargo98 <rmswargo98@gmail.com>
2026-06-07 02:58:32 +0000

    Fix syntax for tools array in download benchmark scripts for Expanse, FABRIC, and local environments

--- run_all_download_benchmarks_ffs_local.sh
+++ run_all_download_benchmarks_ffs_local.sh
@@ -147,7 +147,7 @@ accession_lists=(
     "$ACCESSION_DIR/accessions_small_PRJNA916347.txt"
 )
 
-tools=( "fastbiodl", "pysradb")
+tools=( "fastbiodl" "pysradb" )
 
 total_deleted_bytes=0
 overall_status=0
